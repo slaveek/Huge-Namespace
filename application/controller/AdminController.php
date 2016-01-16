50a3ed62c8f38fe94121361c57fde8cd3000f1c9
Slaveek <slarush@gmail.com>
2016-01-16 15:22:33 +0000

    Upload files

---
+++ application/controller/AdminController.php
@@ -0,0 +1,41 @@
+<?php
+namespace Huge\Controller;
+
+use Huge\Core\Redirect;
+use Huge\Core\Request;
+use Huge\Model\AdminModel;
+use Huge\Model\UserModel;
+
+class AdminController extends \Huge\Core\Controller
+{
+    /**
+     * Construct this object by extending the basic Controller class
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        // special authentication check for the entire controller: Note the check-ADMIN-authentication!
+        // All methods inside this controller are only accessible for admins (= users that have role type 7)
+        \Huge\Core\Auth::checkAdminAuthentication();
+    }
+
+    /**
+     * This method controls what happens when you move to /admin or /admin/index in your app.
+     */
+    public function index()
+    {
+	    $this->View->render('admin/index', array(
+			    'users' => UserModel::getPublicProfilesOfAllUsers())
+	    );
+    }
+
+	public function actionAccountSettings()
+	{
+		AdminModel::setAccountSuspensionAndDeletionStatus(
+			Request::post('suspension'), Request::post('softDelete'), Request::post('user_id')
+		);
+
+		Redirect::to("admin");
+	}
+}
